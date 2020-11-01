d71f4e9742fc430e0d91ebc9cc480f90c65b4c6e
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-11-02 00:43:31 +0100

    Add skeleton files
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'tfc_agent'
+maintainer       'Schuberg Philis'
+maintainer_email 'cookbooks@schubergphilis.com'
+license          'Apache-2.0'
+description      'Installs and configures a Terraform Cloud agent'
+version          '0.1.0'
+chef_version     '>= 15.0'
+
+issues_url 'https://github.com/SBP-Cookbooks/tfc_agent/issues'
+source_url 'https://github.com/SBP-Cookbooks/tfc_agent'
+
+depends 'docker', '~> 7.2'
---
+++ test/integration/default/default_test.rb
@@ -0,0 +1,4 @@
+# InSpec test for recipe tfc_agent::default
+
+# The InSpec reference, with examples and extensive documentation, can be
+# found at https://docs.chef.io/inspec/resources/
