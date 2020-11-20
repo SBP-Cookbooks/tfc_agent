0cd4ce803e2bd1228b4e3b2563fe606df19d00d1
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-11-20 23:08:06 +0100

    Release v0.1.1
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,9 @@
 
 This file is used to list changes made in each version of the tfc_agent cookbook.
 
-## Unreleased
+## 0.1.1 (2020-11-20)
 
-Fix: Made container name unique to allow multiple instances to run on the same host.
+Fix: Make container name unique to allow multiple instances to run on the same host.
 
 ## 0.1.0 (2020-11-09)
 
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Schuberg Philis'
 maintainer_email 'cookbooks@schubergphilis.com'
 license          'Apache-2.0'
 description      'Installs and configures a Terraform Cloud agent'
-version          '0.1.0'
+version          '0.1.1'
 chef_version     '>= 15.0'
 
 issues_url 'https://github.com/SBP-Cookbooks/tfc_agent/issues'
